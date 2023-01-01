c6ba4c4c71dceb2a826b972d27944160965c738c
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2023-01-01 10:27:24 +0100

    build: install Jest depedency to run tests

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", {}],
+  },
+};
+
+export default jestConfig;
